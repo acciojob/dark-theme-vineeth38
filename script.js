d77e1b5dd23aea902aa25512f1cd751f4291e8ad
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-07-31 12:11:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 //your code here
+function swapTheme() {
+    // Get the div and button elements
+    var div = document.getElementById("app");
+    var button = document.getElementById("swap");
 
+    // Check the current class of the div and button, and change it
+    if (div.className === "day") {
+        // Change the class to "night"
+		div.className = "night";
+    } else {
+        // Change the class back to "day"
+		div.className = "day";
+    }
+}
